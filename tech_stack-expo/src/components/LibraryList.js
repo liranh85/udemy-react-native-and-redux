61d0c71e0d0bc7f76cc7c43ed035fe0e5efcc1ce
Liran Harary <liran.harary@somoglobal.com>
2018-08-16 18:35:35 +0100

    tech_stack: display list of technologies from redux state

--- tech_stack-expo/src/components/LibraryList.js
+++ tech_stack-expo/src/components/LibraryList.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import { FlatList } from 'react-native'
 import { connect } from 'react-redux'
+import ListItem from './ListItem'
 
 class LibraryList extends Component {
+  renderItem(library) {
+    return <ListItem library={library} />
+  }
+
   render() {
-    console.log(this.props)
-    return
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={library => library.id.toString()}
+      />
+    )
   }
 }
 
