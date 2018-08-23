83a0d6f8b1916ba9b94c2af7c29f2a4de612782a
Liran Harary <liran.harary@somoglobal.com>
2018-08-23 12:04:37 +0100

    Section 16 complete

---
+++ manager/src/reducers/AuthReducer.js
@@ -0,0 +1,56 @@
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_START,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
+} from '../actions/types'
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  user: null,
+  error: '',
+  loading: false
+}
+
+export default (state = INITIAL_STATE, action) => {
+  console.log(action)
+  switch (action.type) {
+    case EMAIL_CHANGED:
+      return {
+        ...state,
+        email: action.payload
+      }
+
+    case PASSWORD_CHANGED:
+      return {
+        ...state,
+        password: action.payload
+      }
+
+    case LOGIN_USER_START:
+      return {
+        ...state,
+        error: '',
+        loading: true
+      }
+
+    case LOGIN_USER_SUCCESS:
+      return {
+        ...state,
+        ...INITIAL_STATE,
+        user: action.payload,
+      }
+
+    case LOGIN_USER_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: 'Authentication failed'
+      }
+
+    default:
+      return state
+  }
+}
