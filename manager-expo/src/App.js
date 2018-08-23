8980af2542afa10b1a6f537bcc65ed8b34272085
Liran Harary <liran.harary@somoglobal.com>
2018-08-23 16:24:21 +0100

    Remove manager app, create manager-expo app
    Using Expo (create-react-native-app) instead of react-native-cli for the manager app.
    Reason being, this app uses an old version of react-native-router-flux, which doesn't seem to work with the latest version of react-native, meaning I have to use an old version of react-native.
    If I use an old version of react-native, I cannot specify the port in `react-native run-ios`, and it will default to 8081.
    This port is used on my corporate laptop, meaning the app will not run, as the port is in use.
    
    Expo solves this by using a different port by default.

---
+++ manager-expo/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import ReduxThunk from 'redux-thunk'
+import firebase from 'firebase'
+import reducers from './reducers'
+import LoginForm from './components/LoginForm'
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyD2SvzWHz6sAtg8KIjOM14ON56VT6sNduo',
+      authDomain: 'manager-e616b.firebaseapp.com',
+      databaseURL: 'https://manager-e616b.firebaseio.com',
+      projectId: 'manager-e616b',
+      storageBucket: 'manager-e616b.appspot.com',
+      messagingSenderId: '627606483830'
+    }
+    firebase.initializeApp(config)
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+    
+    return (
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    )
+  }
+}
+
+export default App
