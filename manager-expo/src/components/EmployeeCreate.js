9e3c3ff0f2b44e6962a93878ea4ea9ce0435b6c2
Liran Harary <liran.harary@somoglobal.com>
2018-08-24 13:13:46 +0100

    Section 17 complete

--- manager-expo/src/components/EmployeeCreate.js
+++ manager-expo/src/components/EmployeeCreate.js
@@ -1,14 +1,67 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { Picker, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { employeeUpdate } from '../actions'
+import { Card, CardSection, Input, Button } from './common'
 
 class EmployeeCreate extends Component {
   render() {
+    const { name, phone, shift, employeeUpdate } = this.props
+
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label='Name'
+            placeholder='Jane'
+            value={name}
+            onChangeText={value => employeeUpdate({ prop: 'name', value })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label='Phone'
+            placeholder='555-555-5555'
+            value={phone}
+            onChangeText={value => employeeUpdate({ prop: 'phone', value })}
+          />
+        </CardSection>
+        
+        <CardSection style={{ flexDirection: 'column' }}>
+          <Text style={styles.pickerTextStyle} >Shift</Text>
+          <Picker
+            selectedValue={shift}
+            onValueChange={value => employeeUpdate({ prop: 'shift', value })}
+          >
+            <Picker.Item label='Monday' value='' />
+            <Picker.Item label='Tuesday' value='Tuesday' />
+            <Picker.Item label='Wednesday' value='Wednesday' />
+            <Picker.Item label='Thursday' value='Thursday' />
+            <Picker.Item label='Friday' value='Friday' />
+            <Picker.Item label='Saturday' value='Saturday' />
+            <Picker.Item label='Sunday' value='Sunday' />
+          </Picker>
+        </CardSection>
+
+        <CardSection>
+          <Button>Create</Button>
+        </CardSection>
+      </Card>
     )
   }
 }
 
-export default EmployeeCreate
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
+  }
+}
+
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm
+  return { name, phone, shift }
+}
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate)
---
+++ manager-expo/src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,21 @@
+import { EMPLOYEE_UPDATE } from '../actions/types'
+
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+}
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      const { prop, value } = action.payload
+      return {
+        ...state,
+        [prop]: value
+      }
+
+    default:
+      return state
+  }
+}
